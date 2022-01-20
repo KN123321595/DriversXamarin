d6e9c06b2bea760d47bb80a0ecdd4d0a1fd22408
KN123321595 <pet_d2002@mail.ru>
2022-01-20 21:43:29 +0300

    3

--- TestXamarin/TestXamarin/Car.cs
+++ TestXamarin/TestXamarin/Car.cs
@@ -7,10 +7,10 @@ namespace TestXamarin
     public class Car
     {
         public int id { get; set; }
-        public int number { get; set; }
+        public string number { get; set; }
         public int mileage { get; set; }
 
-        public Car(int id, int number, int mileage)
+        public Car(int id, string number, int mileage)
         {
             this.id = id;
             this.number = number;
